22627b8204e0b619e4f62876db58434f04c042f5
developerJhonAlon <jhonnyjami@yahoo.com>
2015-09-08 16:36:32 -0500

    Union de cambios en el Local Jhonny

--- guiaTelefonica/src/beans/AdministradorBean.java
+++ guiaTelefonica/src/beans/AdministradorBean.java
@@ -18,9 +18,10 @@ import org.primefaces.event.UnselectEvent;
 
 import controlador.AsignarAdministradorServicio;
 import controlador.ModificarAdmin;
+
 @ManagedBean
 @ViewScoped
-public class AdministradorBean implements Serializable{
+public class AdministradorBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private List<VistaAdministradores> vistaAdmin;
@@ -29,38 +30,44 @@ public class AdministradorBean implements Serializable{
 	private AsignarAdministradorServicio asignarServicio = new AsignarAdministradorServicio();
 
 	private String valor;
-	private boolean desplegarInf=false;
+	private boolean desplegarInf = false;
 	private VistaAdministradores selectedPersona;
 	private List<Busqueda> listaSedes;
 	private List<Busqueda> todasSedes;
-	
-	List<String> todas = new ArrayList<String>(); 
-	
-	public AdministradorBean()
-	{ }
+
+	List<String> todas = new ArrayList<String>();
+
+	public AdministradorBean() {
+	}
 
 	public List<String> getTodas() {
 		return todas;
 	}
+
 	public void setTodas(List<String> todas) {
 		this.todas = todas;
 	}
-	
+
 	public ModificarAdmin getModificarAdmin() {
 		return modificarAdmin;
 	}
+
 	public void setModificarAdmin(ModificarAdmin modificarAdmin) {
 		this.modificarAdmin = modificarAdmin;
 	}
+
 	public AsignarAdministradorServicio getAsignarServicio() {
 		return asignarServicio;
 	}
+
 	public void setAsignarServicio(AsignarAdministradorServicio asignarServicio) {
 		this.asignarServicio = asignarServicio;
 	}
+
 	public List<Busqueda> getTodasSedes() {
 		return todasSedes;
 	}
+
 	public void setTodasSedes(List<Busqueda> todasSedes) {
 		this.todasSedes = todasSedes;
 	}
@@ -96,35 +103,34 @@ public class AdministradorBean implements Serializable{
 	public void setValor(String valor) {
 		this.valor = valor;
 	}
-	
-	@PostConstruct 
+
+	@PostConstruct
 	public void consultar() {
 		System.out.println("OBTENER ADMINISTRADORES --->>");
-		this.vistaAdmin = modificarAdmin.obtenerAdmin(); 
+		this.vistaAdmin = modificarAdmin.obtenerAdmin();
 	}
-	
-	public void consultarUno()
-	{
+
+	public void consultarUno() {
 		System.out.println("OBTENER UN ADMINISTRADOR --->>");
-		this.vistaUnAdmin = modificarAdmin.obtenerUnAdmin(this.valor); 
+		this.vistaUnAdmin = modificarAdmin.obtenerUnAdmin(this.valor);
 	}
 
 	public List<VistaAdministradores> getVistaAdmin() {
 		return vistaAdmin;
 	}
 
-
 	public void setVistaAdmin(List<VistaAdministradores> vistaAdmin) {
 		this.vistaAdmin = vistaAdmin;
 	}
-	
+
 	public List<Busqueda> getVistaUnAdmin() {
 		return vistaUnAdmin;
 	}
+
 	public void setVistaUnAdmin(List<Busqueda> vistaUnAdmin) {
 		this.vistaUnAdmin = vistaUnAdmin;
 	}
-	
+
 	public void addMessage(String summary) {
 		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
 				summary, null);
@@ -132,36 +138,37 @@ public class AdministradorBean implements Serializable{
 	}
 
 	public void onRowSelect(SelectEvent event) {
-		
+
 		System.out.println("Seleccion Row ");
-		
-		this.desplegarInf = true;	
-		 this.todas.clear();
+
+		this.desplegarInf = true;
+		this.todas.clear();
 
 		this.todasSedes = this.asignarServicio.obtenerSedes();
-		
-		
-		this.vistaUnAdmin=this.modificarAdmin.obtenerUnAdmin(this.selectedPersona.getId_persona());
-        
-	    for (Busqueda busqueda : vistaUnAdmin) {
+
+		this.vistaUnAdmin = this.modificarAdmin
+				.obtenerUnAdmin(this.selectedPersona.getId_persona());
+
+		for (Busqueda busqueda : vistaUnAdmin) {
 			this.todas.add(busqueda.getDescripcion());
 		}
-    }
+	}
 
-    public void onRowUnselect(UnselectEvent event) {
-    	System.out.println("Seleccion Row ");
-    	this.desplegarInf = false;
-    }
-    
-    public void Modificar() {	
-		modificarAdmin.modificar(this.selectedPersona, todas); 
+	public void onRowUnselect(UnselectEvent event) {
+		System.out.println("Seleccion Row ");
+		this.desplegarInf = false;
+		this.todas.clear();
+	}
+
+	public void Modificar() {
+		modificarAdmin.modificar(this.selectedPersona, todas);
 		addMessage("Información Modificada...");
 	}
-    
-    public void Eliminar() {	
-    	modificarAdmin.eliminar(this.selectedPersona); 
+
+	public void Eliminar() {
+		modificarAdmin.eliminar(this.selectedPersona);
 		addMessage("Información Eliminada...");
 		this.vistaAdmin.clear();
-		this.vistaAdmin = modificarAdmin.obtenerAdmin(); 
+		this.vistaAdmin = modificarAdmin.obtenerAdmin();
 	}
 }
